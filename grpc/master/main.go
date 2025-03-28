07f38b2d36493e3826f3a677898c3834e4f7e34f
fatmaebrahim <fatmaebrahim3131@gmail.com>
2025-03-28 04:27:19 +0200

    feat: added replicate proto and helper functions

--- grpc/master/main.go
+++ grpc/master/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net"
+	"time"
+
 	// "strings"
 
 	pb "github.com/RawanMostafa08/Distributed-File-System/grpc/Upload"
@@ -18,6 +21,7 @@ type DataNode struct {
 }
 
 type FileData struct {
+	FileID int
 	Filename string
 	FilePath string
 	FileSize int64
@@ -29,10 +33,13 @@ type LookUpTableTuple struct {
 }
 
 
+
+
 type textServer struct {
 	pb.UnimplementedDFSServer
 }
 var lookupTuple LookUpTableTuple
+var dataNodes [] DataNode 
 
 func (s *textServer) DownloadPortsRequest(ctx context.Context, req *pb.DownloadPortsRequestBody) (*pb.DownloadPortsResponseBody, error) {
 	fmt.Println("DownloadPortsRequest called")
@@ -41,11 +48,11 @@ func (s *textServer) DownloadPortsRequest(ctx context.Context, req *pb.DownloadP
 	file_size = 0
 	fmt.Println(req.GetFileName()) 
 	// ##########################################################################
-	// Dummy Table Should be removed later 
+	// Dummy Table Should be removed later , added id for each file
 	lookupTuple := LookUpTableTuple{
 		File: []FileData{
-			{Filename: "file1.mp4", FilePath: "grpc\\files\\file1.mp4" , FileSize:1055736 , Node: DataNode{DataKeeperNode: ":3000", IsDataNodeAlive: true}},
-			{Filename: "file1.mp4", FilePath: "grpc\\files\\file1.mp4", FileSize:1055736 , Node: DataNode{DataKeeperNode: ":8090", IsDataNodeAlive: true}},
+			{FileID: 1, Filename: "file1.mp4", FilePath: "grpc\\files\\file1.mp4" , FileSize:1055736 , Node: DataNode{DataKeeperNode: ":3000", IsDataNodeAlive: true}},
+			{FileID: 2, Filename: "file1.mp4", FilePath: "grpc\\files\\file1.mp4", FileSize:1055736 , Node: DataNode{DataKeeperNode: ":8090", IsDataNodeAlive: true}},
 		},
 	}
 	// ##########################################################################
@@ -63,6 +70,81 @@ func (s *textServer) DownloadPortsRequest(ctx context.Context, req *pb.DownloadP
 	return &pb.DownloadPortsResponseBody{Addresses: nodes, FileSize: file_size }, nil
 }
 
+// Replicate Helper Functions
+func getFileNodes(fileID int)  []string {
+
+	nodes := []string{}
+	for _, f := range lookupTuple.File {
+		if f.FileID == fileID && f.Node.IsDataNodeAlive == true {
+			nodes = append(nodes, f.Node.DataKeeperNode )
+		}
+	}
+	return  nodes
+}
+
+func selectNodeToCopyTo(fileID int, fileNodes []string) (string,error) {
+	// alive node , not in the list of nodes that have the file
+	validNodes := []string{}
+	for _,node := range dataNodes {
+		if node.IsDataNodeAlive == true  {
+			for _, fileNode := range fileNodes {
+				if fileNode == node.DataKeeperNode {
+					continue
+				}
+				validNodes = append(validNodes, node.DataKeeperNode)
+			}
+
+		}
+	}
+	if len(validNodes) == 0 {
+		return "",errors.New("No valid nodes to copy to")
+	}
+	return validNodes[0],nil
+	
+
+}
+
+func CopyFileToNode(file FileData, srcNode string, destNode string) error {
+	//notify machines
+	//copy file
+	//update lookup table 
+	
+	for _,f := range lookupTuple.File {
+		if f.FileID == file.FileID {
+			f.Node.DataKeeperNode = destNode
+		}
+	}
+	return nil
+}
+
+
+// assume that each file record has fileid can be replicated in lookup table
+// fileid 1, file1, node1
+// fileid 1, file1, node2
+// fileid 2, file1, node3
+// fileid 2, file1, node1
+func ReplicateFile() {
+	for {
+		time.Sleep(10 * time.Second)
+		for _, file := range lookupTuple.File {
+			// get all nodes that have this file
+			nodes := getFileNodes(file.FileID);
+			if len(nodes) < 3 && len(nodes) > 0{
+					valid, err :=selectNodeToCopyTo(file.FileID,nodes)
+					if err != nil {
+						fmt.Errorf("Error selecting node to copy to")
+					}
+					fmt.Println("Selected Node to copy to: ", valid)
+					err = CopyFileToNode(file, nodes[0], valid)
+					if err != nil {
+						fmt.Errorf("Error copying file")
+					}
+			}
+		}
+
+	}
+}
+
 func main() {
 
 	
@@ -77,12 +159,15 @@ func main() {
 		return
 	}
 
+	go ReplicateFile()
+
 	s := grpc.NewServer()
 	pb.RegisterDFSServer(s, &textServer{})
 	fmt.Println("Server started. Listening on port 8080...")
 	if err := s.Serve(lis); err != nil {
 		fmt.Println("failed to serve:", err)
 	}
+	
 	
 
 }
