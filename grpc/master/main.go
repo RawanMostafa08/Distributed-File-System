f0bf5914abaee9c27e3d7095d114d81eb1cedea9
mennamohamed0207 <menamohamed0207@gmail.com>
2025-03-29 03:07:59 +0200

    feat: add ping.proto

--- grpc/master/main.go
+++ grpc/master/main.go
@@ -2,75 +2,113 @@ package main
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"net"
 	// "strings"
 
 	pb "github.com/RawanMostafa08/Distributed-File-System/grpc/Upload"
 	pbUtils "github.com/RawanMostafa08/Distributed-File-System/grpc/utils"
+	pbHeartBeats "github.com/RawanMostafa08/Distributed-File-System/grpc/HeartBeats"
+
+	
 	"google.golang.org/grpc"
 	// "google.golang.org/grpc/peer"
 )
 
 type DataNode struct {
-	DataKeeperNode string
+	IP              string
+	Port            string
+	NodeID          string
 	IsDataNodeAlive bool
 }
 
 type FileData struct {
+	FileID   string
 	Filename string
 	FilePath string
 	FileSize int64
-	Node DataNode
+	NodeID   string
 }
 
 type LookUpTableTuple struct {
 	File []FileData
 }
 
-
 type textServer struct {
 	pb.UnimplementedDFSServer
 }
+
 var lookupTuple LookUpTableTuple
+var dataNodes []DataNode
+
+var lookupTable = []FileData{
+	{
+		FileID:   "file_1",
+		Filename: "data1.txt",
+		FilePath: "/data/files/data1.txt",
+		FileSize: 1024,
+		NodeID:   "Node_1",
+	},
+	{
+		FileID:   "file_2",
+		Filename: "data2.txt",
+		FilePath: "/data/files/data2.txt",
+		FileSize: 2048,
+		NodeID:   "Node_2",
+	},
+}
+
+func getNodeByID(nodeID string) (DataNode, error) {
+	for _, node := range dataNodes {
+		if node.NodeID == nodeID {
+			return node, nil
+		}
+	}
+	return DataNode{}, errors.New("Node not found")
+}
 
 func (s *textServer) DownloadPortsRequest(ctx context.Context, req *pb.DownloadPortsRequestBody) (*pb.DownloadPortsResponseBody, error) {
 	fmt.Println("DownloadPortsRequest called")
-	var nodes [] string 
+	var nodes []string
 	var file_size int64
 	file_size = 0
-	fmt.Println(req.GetFileName()) 
+	fmt.Println(req.GetFileName())
 	// ##########################################################################
-	// Dummy Table Should be removed later 
-	lookupTuple := LookUpTableTuple{
-		File: []FileData{
-			{Filename: "file1.mp4", FilePath: "grpc\\files\\file1.mp4" , FileSize:1055736 , Node: DataNode{DataKeeperNode: ":3000", IsDataNodeAlive: true}},
-			{Filename: "file1.mp4", FilePath: "grpc\\files\\file1.mp4", FileSize:1055736 , Node: DataNode{DataKeeperNode: ":8090", IsDataNodeAlive: true}},
-		},
-	}
-	// ##########################################################################
+	// Dummy Table Should be removed later
+	// lookupTuple := LookUpTableTuple{
+	// 	File: []FileData{
+	// 		{Filename: "file1.mp4", FilePath: "grpc\\files\\file1.mp4" , FileSize:1055736 , Node: DataNode{DataKeeperNode: ":3000", IsDataNodeAlive: true}},
+	// 		{Filename: "file1.mp4", FilePath: "grpc\\files\\file1.mp4", FileSize:1055736 , Node: DataNode{DataKeeperNode: ":8090", IsDataNodeAlive: true}},
+	// 	},
+	// }
+	// // ##########################################################################
 
-	for _, file := range lookupTuple.File {
-		fmt.Println(file.Filename + " " + req.GetFileName()) 
-		if file.Filename == req.GetFileName() {
-			if file.Node.IsDataNodeAlive == true {
-				nodes = append(nodes, file.Node.DataKeeperNode)
-				file_size = file.FileSize
-			}
-		}
-	}
+	// for _, file := range lookupTuple.File {
+	// 	fmt.Println(file.Filename + " " + req.GetFileName())
+	// 	if file.Filename == req.GetFileName() {
+	// 		if file.Node.IsDataNodeAlive == true {
+	// 			nodes = append(nodes, file.Node.DataKeeperNode)
+	// 			file_size = file.FileSize
+	// 		}
+	// 	}
+	// }
 
-	return &pb.DownloadPortsResponseBody{Addresses: nodes, FileSize: file_size }, nil
+	return &pb.DownloadPortsResponseBody{Addresses: nodes, FileSize: file_size}, nil
 }
 
 func main() {
 
-	
 	fmt.Println("master started...")
 	var masterAddress, clientAddress string
 	nodes := []string{}
-	pbUtils.ReadFile(&masterAddress,&clientAddress,&nodes)
-	
+	pbUtils.ReadFile(&masterAddress, &clientAddress, &nodes)
+
+	for i, node := range nodes {
+		dataNodes = append(dataNodes, DataNode{Port: node, NodeID: fmt.Sprintf("Node_%d", i), IsDataNodeAlive: true})
+	}
+	// check if nodes are alive and update the dataNodes list
+
 	lis, err := net.Listen("tcp", masterAddress)
 	if err != nil {
 		fmt.Println("failed to listen:", err)
@@ -83,6 +121,5 @@ func main() {
 	if err := s.Serve(lis); err != nil {
 		fmt.Println("failed to serve:", err)
 	}
-	
 
 }
