8ac4307163c05ab6cd92ace08410adf0d2aa150a
RawanMostafa08 <rawanmostafa401@gmail.com>
2025-04-12 12:19:20 +0200

    chore: added prints for node

--- grpc/node/main.go
+++ grpc/node/main.go
@@ -57,6 +57,7 @@ func (s *textServer) DownloadFileRequest(ctx context.Context, req *pb.DownloadFi
 	// Open the file
 	// filePath := fmt.Sprintf("%s/%s", req.FilePath, req.FileName)
 	filePath := filepath.Join(req.FilePath, req.FileName)
+	fmt.Printf("filePathhhhhhhhhhhhhhhhhhhhhhh in download %s\n", filePath)
 	file, err := os.Open(filePath)
 	if err != nil {
 		return nil, err
@@ -101,6 +102,8 @@ func (s *textServer) UploadFileRequest(ctx context.Context, req *pb.UploadFileRe
 	}
 
 	filePathU := filepath.Join("files", s.nodeID, req.FileName)
+	fmt.Printf("filePathhhhhhhhhhhhhhhhhhhhhhh in upload %s\n", filePathU)
+
 	if err := os.WriteFile(filePathU, req.FileData, 0644); err != nil {
 		return nil, fmt.Errorf("failed to save file: %v", err)
 	}
@@ -182,7 +185,7 @@ func (s *replicateServer) CopyFile(ctx context.Context, req *pb_r.CopyFileReques
 		fmt.Println("Error writing file:", err)
 		return nil, err
 	}
-	
+
 	fmt.Println("File copied successfully to:", req.DestId)
 	return &pb_r.CopyFileResponse{Ack: "ACK"}, nil
 }
