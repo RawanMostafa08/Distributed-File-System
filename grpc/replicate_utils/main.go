6e0cc2d7525702b0826dac6b275f1a8be6187a79
Sara Bisheer <113303945+SH8664@users.noreply.github.com>
2025-04-13 21:06:05 +0200

    fix: remove print

--- grpc/replicate_utils/main.go
+++ grpc/replicate_utils/main.go
@@ -26,7 +26,6 @@ func GetAvailablePort(node models.DataNode) (string, error) {
 	for _, port := range node.Port {
 		address := fmt.Sprintf("%s:%s", node.IP, port)
 		conn, err := grpc.Dial(address, grpc.WithInsecure())
-		fmt.Printf("trying to dial to address: %s\n", address)
 		if err != nil {
 			conn.Close()
 			continue
